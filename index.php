e038cd45a376fdafb55efaf3c54c4e27294f8770
vichea_unlimited <kimsreymommak@gmail.com>
2024-07-03 14:02:57 +0700

    Website

---
+++ index.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+
+<title>Website Movies</title>
+    <link rel="stylesheet" href="css/style.css"/>	
+    <link rel="Shortcut icon" href="images/fav icon gg.png">
+</head>
+<body>
+    <nav>
+        <a href="" class="logo">
+            <img src="images/gjgkgjkfdffjjyfuyfkuxt,.png">
+        </a>
+        <label class="menu-icon" for="menu-btn">
+            <span class="nav-icon"></span>
+        </label>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">KhmerDub</a></li>
+            <li><a href="#">Chinese</a></li>
+            <li><a href="#">Popular Movies</a></li>
+        </ul>
+        <div class="search">
+            <input type="text" placeholder="Search"/>
+            <i class="fas fa-search"></i>
+        </div>
+    </nav>
+    <div class="movies-heading">
+    </div>
+    <section id="movies-list">
+        <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/F9_film_poster.jpg">
+            </div>
+            <a href="#">
+               F9
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/download.jpg">
+            </div>
+            <a href="#">
+                Space Jum
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/l-1 (2).jpg">
+            </div>
+            <a href="#">
+                Snake Eyes
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/l-1 (3).jpg">
+            </div>
+            <a href="#">
+                black Window
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg">
+            </div>
+            <a href="#">
+                Avengers
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/download (2).jpg">
+            </div>
+            <a href="#">
+                Shang-Chi 
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/avengersendgame_lob_crd_05.jpg">
+            </div>
+            <a href="#">
+                Avengers End Game
+            </a>
+        </div>
+         <div class="movies-box">
+            <div class="movies-img">
+                <img src="images/download (1).jpg">
+            </div>
+            <a href="#">
+               Loki
+            </a>
+        </div>
+    </section>
+    <footer>
+        <p> សួស្ដីបងប្អូនហូបបាយនៅ.Ltd Zin Two BC ZIN</p>
+    </footer>
+</body>
+</html>
